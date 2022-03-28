6ebfaca217013c135a820ab3f5d52843f71d02ae
Allison Anguiano <aanguiano@dev-10.com>
2022-03-28 11:32:46 -0500

    Going back to dev

--- SolarFarm.DAL/PanelRepository.cs
+++ SolarFarm.DAL/PanelRepository.cs
@@ -7,112 +7,119 @@ using SolarFarm.Core;
 
 namespace SolarFarm.DAL
 {
-    public class PanelRepository : IPanelRepository         //do I need an interface for the repository?
+    public class PanelRepository : IPanelRepository        
     {
         private List<Panel> _panels;        
 
 
         //public PanelRepository()                    //for testing purposes
-        //{ }
-             public string Path { get; set; }
-             public IPanelFormatter fmt { get; set; }
-        
-            public Result<List<Panel>> GetAll()
+        //{
+        //}
+             public string Path { get; set; }             
+             public IPanelFormatter Fmt { get; set; }
+
+             //string csvPath = Directory.GetCurrentDirectory() + @"\Data\Panels.csv";    //testing
+             //IPanelFormatter csvFormat = new PanelCSVFormatter();
+
+        //PanelRepository csv.Fmt = csvFormat;
+        //csv.Path = csvPath;
+        public Result<List<Panel>> GetAll()
+        {
+            
+            _panels = new();
+            Result<List<Panel>> result = new Result<List<Panel>>();
+
+            if (File.Exists(csvPath))
             {
-                _panels = new();         
-                Result<List<Panel>> result = new Result<List<Panel>>();            
-
-                if (File.Exists(Path))
+                using (StreamReader sr = new StreamReader(csvPath))    //testing
                 {
-                    using (StreamReader sr = new StreamReader(Path))
+                    string line = sr.ReadLine();
+                    if (line != null && Fmt.HasHeaderLine())    //exception thrown 
                     {
-                        string line = sr.ReadLine();
-                        if (line != null && fmt.HasHeaderLine())
-                        {
-                            line = sr.ReadLine();
-                        }
+                        line = sr.ReadLine();
+                    }
 
-                        while (line != null)
-                        {
-                            _panels.Add(fmt.Deserialize(line));
-                            line = sr.ReadLine();
-                        }
+                    while (line != null)
+                    {
+                        _panels.Add(Fmt.Deserialize(line));
+                        line = sr.ReadLine();
                     }
                 }
-                else
-                {
-                    Console.WriteLine($"Cannot find {Path}");
-                }
-
-                result.Success = true;
-                result.Message = "";
-                result.Data = _panels;
-                //result.Data = new List<Panel>(_panels);
-                return result;
+            }
+            else
+            {
+                Console.WriteLine($"Cannot find {csvPath}");
             }
 
-        public void WriteAll(List<Panel> panels)
+            result.Success = true;
+            result.Message = "";
+            result.Data = _panels;
+            //result.Data = new List<Panel>(_panels);
+            return result;
+        }
+
+        public void WriteAll(List<Panel> _panels)   //hmmm
         {
-            using (StreamWriter sw = new StreamWriter(Path))    //HERE
+            using (StreamWriter sw = new StreamWriter(csvPath))    //HERE
             {
-                if (fmt.HasHeaderLine())
+                if (Fmt.HasHeaderLine())
                 {
-                    sw.WriteLine(fmt.HeaderLine());
+                    sw.WriteLine(Fmt.HeaderLine());
                 }
 
-                foreach (Panel p in panels)
+                foreach (Panel p in _panels)
                 {
-                    sw.WriteLine(fmt.Serialize(p));
+                    sw.WriteLine(Fmt.Serialize(p));
                 }
             }
         }
         //Work on ConsoleIO and serialization  (need package for serial)          
 
-        //    string path = Directory.GetCurrentDirectory() + @"\Data\SolarPanels.csv";
+        //        string path = Directory.GetCurrentDirectory() + @"\Data\Panels.csv";
 
-        //    if (File.Exists(path))
-        //    {
-        //        // to hold data
-        //        List<Panel> panels = new List<Panel>();
-
-        //        // create and open streamreader
-        //        using (StreamReader sr = new StreamReader(path))
-        //        {
-        //            string currentLine = sr.ReadLine(); // header line
-        //                                                //maybe make variable for head row... or actually don't have to
-        //            ///ask if there is a header
-        //            currentLine = sr.ReadLine(); // 1st data row
-
-        //            while (currentLine != null)
+        //            if (File.Exists(path))
         //            {
-        //                Panel p = new Panel(); // create a panel object for each line
-        //                string[] columns = currentLine.Split(","); // split columns
+        //                // to hold data
+        //                List<Panel> panels = new List<Panel>();
 
-        //                // add data to object, parsing types as necessary
-        //                p.Section = columns[0];
-        //                p.Row = int.Parse(columns[1]);
-        //                p.Column = int.Parse(columns[2]);
-        //                string month = "1/1/";
-        //                string yearString = columns[3];
-        //                p.Year = DateTime.Parse(month + yearString);
-        //                p.Material = int.Parse(columns[4]);
-        //                p.IsTracking = columns[5];
+        //                // create and open streamreader
+        //                using (StreamReader sr = new StreamReader(path))
+        //                {
+        //                    string currentLine = sr.ReadLine(); // header line
+        //                                                        //maybe make variable for head row... or actually don't have to
+        //    ///ask if there is a header
+        //    currentLine = sr.ReadLine(); // 1st data row
 
-        //                panels.Add(p); // add object to list
+        //                    while (currentLine != null)
+        //                    {
+        //                        Panel p = new Panel(); // create a panel object for each line
+        //    string[] columns = currentLine.Split(","); // split columns
 
-        //                currentLine = sr.ReadLine(); // next line
+        //    // add data to object, parsing types as necessary
+        //    p.Section = columns[0];
+        //                        p.Row = int.Parse(columns[1]);
+        //    p.Column = int.Parse(columns[2]);
+        //    string month = "1/1/";
+        //    string yearString = columns[3];
+        //    p.Year = DateTime.Parse(month + yearString);
+        //                        p.Material = int.Parse(columns[4]);
+        //    p.IsTracking = columns[5];
+
+        //                        panels.Add(p); // add object to list
+
+        //                        currentLine = sr.ReadLine(); // next line
+        //                    }
+        //                }
         //            }
-        //        }
-        //    }
 
-        //    //string wholeFile = File.ReadAllText(path);
+        //            //string wholeFile = File.ReadAllText(path);
 
         //    //Console.WriteLine(wholeFile);
-        //    else
-        //    {
-        //            Create(path); //perhaps? create csv file
-        //    }
+        //else
+        //{
+        //    Console.WriteLine($"File at {path} not found.");
         //}
+        //        }
 
         public Result<Panel> Add(Panel panel)
             {
